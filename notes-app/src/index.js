ba819e10e92fc8ffb62959b76fd296d731a44cd8
Satya Vamsi Moganti <vamsee1993@gmail.com>
2019-11-17 20:28:33 -0700

    updates to app

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -19,6 +19,29 @@ class NotesApp extends React.Component {
     };
   };
 
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+      // Do nothing at all
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+
+  componentWillUnmount(){
+    console.log("Component will unmount");
+  };
+
   handleRemoveAll() {
     this.setState(() =>({  options : []}));
   };
